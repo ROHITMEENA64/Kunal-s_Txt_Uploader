e6cbd86cdebcd1d4a66570af6fa6e32291c7a309
ROHITMEENA64 <157285469+ROHITMEENA64@users.noreply.github.com>
2025-04-06 21:21:27 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "27994649"
-api_hash = "a667f9bf86b1e9af6ee9862fee3001e4"
-bot_token = "6767726971:AAG0kQTgbAXaqyd0kd1_tR0vdiAO21J48eQ"
+api_id = "27775431"
+api_hash = "b70bb1d45a1d05236671d4cc615e40f9"
+bot_token = ""
